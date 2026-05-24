7aae635f872815adcb05db182a28c63e8f5cd9ac
FelixDws <felix.dharma34@gmail.com>
2026-05-24 20:35:42 +0700

    Remove next cache

--- frontend/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"9I8wiM8JTZenz+RJrrrrFVrFl8uQAgIHkWn3FpSJoP4=\"\n}"
--- frontend/.next/dev/static/chunks/app_layout_tsx_004glpo._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/styles_globals_0rhi7k0.css","static/chunks/node_modules_0f4awuk._.js"],
-    source: "dynamic"
-});
