d71ffc4cf973f1437092adc2f4269c08e18dbf22
FelixDws <felix.dharma34@gmail.com>
2026-05-24 16:52:19 +0700

    Initial commit

--- backend/src/middlewares/rateLimit.middleware.ts
+++ backend/src/middlewares/rateLimit.middleware.ts
@@ -1,11 +1,34 @@
-import rateLimit from "express-rate-limit"
+import rateLimit
+  from "express-rate-limit"
 
-export const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
+export const limiter =
+  rateLimit({
 
-  message: {
-    success: false,
-    message: "Too many requests",
-  },
-})
+    windowMs:
+      15 * 60 * 1000,
+
+    max: 100,
+
+    standardHeaders: true,
+
+    legacyHeaders: false,
+
+    keyGenerator: (req) => {
+
+      return (
+        req.headers[
+          "x-forwarded-for"
+        ] as string
+      ) ||
+      req.ip ||
+      "unknown-ip"
+
+    },
+
+    message: {
+      success: false,
+      message:
+        "Too many requests",
+    },
+
+  })
